c54714f95e9b9d22792a50f77fce8a18a8f9215b
Richie <yourfriend2428@gmail.com>
2024-06-18 15:51:10 +0800

    First Lesson. Created a basic GUI that has the text: "Hello World".

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# Creating root widget
+root = Tk()
+
+# Creating a label widget
+myLabel = Label(root, text="Hello World")
+
+# Put it on the screen
+myLabel.pack()
+
+# Starts the GUI
+root.mainloop()
